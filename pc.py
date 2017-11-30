b376f24c246c62fbb3059d224dc97279cb2d51cd
Josh Long <starbuxman@gmail.com>
2017-11-30 10:35:31 -0600

    polish

--- pc.py
+++ pc.py
@@ -6,26 +6,6 @@ import time
 
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s', )
 
-
-class CountDownLatch(object):
-    def __init__(self, count=1):
-        self.count = count
-        self.lock = threading.Condition()
-
-    def count_down(self):
-        self.lock.acquire()
-        self.count -= 1
-        if self.count <= 0:
-            self.lock.notifyAll()
-        self.lock.release()
-
-    def await(self):
-        self.lock.acquire()
-        while self.count > 0:
-            self.lock.wait()
-        self.lock.release()
-
-
 if __name__ == '__main__':
 
     cv = threading.Condition()
@@ -34,6 +14,7 @@ if __name__ == '__main__':
 
     finished = []
 
+
     def handler(ctr):
         logging.debug('entering %s' % ctr)
         seconds = random.randint(1, 10)
@@ -41,7 +22,7 @@ if __name__ == '__main__':
         logging.debug('slept %s!' % seconds)
         with cv:
             finished.append(ctr)
-            if len(finished) == max :
+            if len(finished) == max:
                 logging.debug('finished length=%s - notifying all' % len(finished))
                 cv.notify_all()
 
