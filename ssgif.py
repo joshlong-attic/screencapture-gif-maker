cf4c45b73b80dffbac7783d223452c5a1f4e39d7
Josh Long <starbuxman@gmail.com>
2017-11-29 17:28:05 -0800

    cleanup

--- ssgif.py
+++ ssgif.py
@@ -2,23 +2,19 @@
 
 import os
 import time
-import moviepy.video.io.ImageSequenceClip as isc
 
 import concurrent.futures
+import moviepy.video.io.ImageSequenceClip as isc
 import pyscreenshot as screen
 
-
 class Capture(object):
-    """
-    Takes screenshots at a configured interval
-    """
+
     DEFAULT_FPS = 30
 
     def __init__(self, root_dir, fps=DEFAULT_FPS, executor=concurrent.futures.ThreadPoolExecutor(max_workers=60)):
         super(Capture, self).__init__()
         self.executor = executor
         self.root_dir = root_dir
-
         self.fps = fps
         self.interval = 1.0 / (self.fps * 1.0)
 
@@ -28,11 +24,7 @@ class Capture(object):
         assert os.path.exists(self.root_dir), 'the directory %s must exist.' % self.root_dir
 
     def transcode(self):
-
-        # uses information from https://zulko.github.io/moviepy/getting_started/videoclips.html
-
         files = [os.path.join(self.root_dir, f) for f in os.listdir(self.root_dir) if f.endswith('.png')]
-        print files
         clip = isc.ImageSequenceClip(files, fps=30)
         clip.write_gif(os.path.join(self.root_dir, '../out.gif'))
 
