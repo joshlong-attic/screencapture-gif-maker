28eb36f6ce05f92e5d510ee85aaf62a72e5d74cb
Josh Long <starbuxman@gmail.com>
2017-11-29 18:43:20 -0800

    polish

--- ssgif.py
+++ ssgif.py
@@ -2,7 +2,7 @@
 
 import os
 import time
-
+import threading
 import concurrent.futures
 import moviepy.video.io.ImageSequenceClip as isc
 import pyscreenshot as screen
@@ -11,31 +11,40 @@ import pyscreenshot as screen
 class Capture(object):
     DEFAULT_FPS = 30
 
-    def __init__(self, root_dir, fps=DEFAULT_FPS, executor=concurrent.futures.ThreadPoolExecutor(max_workers=60)):
+    def __init__(
+            self,
+            root_dir,
+            fps=DEFAULT_FPS,
+            executor=concurrent.futures.ThreadPoolExecutor(max_workers=4),
+            should_continue=lambda ctx: ctx['frame'] < 30):
         super(Capture, self).__init__()
         self.executor = executor
         self.root_dir = root_dir
         self.fps = fps
         self.interval = 1.0 / (self.fps * 1.0)
+        self.should_continue = should_continue
 
         if not os.path.exists(self.root_dir):
             os.makedirs(self.root_dir)
 
-        assert os.path.exists(self.root_dir), 'the directory %s must exist.' % self.root_dir
+        assert os.path.exists(
+            self.root_dir), 'the directory %s must exist.' % self.root_dir
 
     def loop(self):
+
+        frame_ctr = 0
 
         def capture(uid):
             file_name = os.path.join(self.root_dir, '%s.png' % uid)
             screen.grab_to_file(file_name)
 
-        ctr = 0
-        while ctr < (30):
-            self.executor.submit(lambda *args: capture(ctr))
-            ctr += 1
+        while self.should_continue({'frame': frame_ctr}):
+            self.executor.submit(lambda *args: capture(frame_ctr))
+            frame_ctr += 1
             time.sleep(self.interval)
 
     def transcode(self):
+        # todo this MUST run only after all the submitted capture threads finish
         files = [os.path.join(self.root_dir, f) for f in os.listdir(self.root_dir) if f.endswith('.png')]
         clip = isc.ImageSequenceClip(files, fps=30)
         clip.write_gif(os.path.join(self.root_dir, '../out.gif'))
@@ -47,3 +56,4 @@ if __name__ == '__main__':
     capture = Capture(ss_dir)
     capture.loop()
     capture.transcode()
+    print 'finished!'
