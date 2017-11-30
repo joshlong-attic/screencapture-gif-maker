633a9036d76f34a76296de5d5487dd9f7ab80f9c
Josh Long <starbuxman@gmail.com>
2017-11-30 11:45:53 -0600

    polish

--- ssgif.py
+++ ssgif.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import threading
 import time
@@ -7,12 +8,9 @@ import time
 import concurrent.futures
 import moviepy.video.io.ImageSequenceClip as isc
 import pyscreenshot as screen
-import logging
 
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s', )
 
-
-# TODO ensure that all threadsd finish BEFORE #transcode is called. research http://effbot.org/zone/thread-synchronization.htm and https://stackoverflow.com/questions/10236947/does-python-have-a-similar-control-mechanism-to-javas-countdownlatch/24796823#24796823
 
 class ScreenRecorder(object):
     DEFAULT_FPS = 30
@@ -28,7 +26,9 @@ class ScreenRecorder(object):
         self.root_dir = root_dir
         self.fps = fps
         self.interval = 1.0 / (self.fps * 1.0)
-        self.should_continue = should_continue
+        self.should_continue_test = should_continue
+        self.should_continue = False
+        self.transcoding = False
 
         if not os.path.exists(self.root_dir):
             os.makedirs(self.root_dir)
@@ -37,17 +37,36 @@ class ScreenRecorder(object):
 
     def record(self, output_file):
 
+        cv = threading.Condition()
 
         frame_ctr = 0
 
         def capture(uid):
+            logging.debug('entering capture handler for %s' % uid)
             file_name = os.path.join(self.root_dir, '%s.png' % uid)
             screen.grab_to_file(file_name)
 
-        while self.should_continue({'frame': frame_ctr}):
+            with cv:
+                if not self.should_continue and not self.transcoding:
+                    logging.debug('notifying that we are done')
+                    cv.notify_all()
+
+        self.should_continue = True
+
+        while self.should_continue_test({'frame': frame_ctr}):
             self.executor.submit(lambda *args: capture(frame_ctr))
             frame_ctr += 1
             time.sleep(self.interval)
+
+        self.should_continue = False
+
+        logging.debug('waiting for all outstanding executors to finish')
+        with cv:
+            cv.wait()
+
+        self.transcoding = True
+
+        logging.debug('transcoding!')
 
         files = [os.path.join(self.root_dir, f) for f in os.listdir(self.root_dir) if f.endswith('.png')]
         clip = isc.ImageSequenceClip(files, fps=30)
@@ -60,5 +79,3 @@ if __name__ == '__main__':
 
     capture = ScreenRecorder(tmp_dir)
     capture.record(gif)
-
-    print 'finished!'
